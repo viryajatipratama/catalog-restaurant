fc56feaf0cb7feb60f347814dc4da7fd72150dee
viryajatipratama <101871221+viryajatipratama@users.noreply.github.com>
2022-10-30 18:01:03 +0700

    Add files via upload
    Catalog-restaurant

---
+++ src/scripts/index.js
@@ -0,0 +1,16 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+const hamburgerButtonElement = document.querySelector('#hamburger');
+const drawerElement = document.querySelector('#drawer');
+const mainElement = document.querySelector('main');
+
+hamburgerButtonElement.addEventListener('click', event => {
+  drawerElement.classList.toggle('open');
+  event.stopPropagation();
+});
+
+
+mainElement.addEventListener('click', event => {
+    drawerElement.classList.remove('open');
+    event.stopPropagation();
+  });
